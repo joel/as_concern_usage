bc2b0809decf8aca8849dc4f03ba133eb1b0e6dc
Joel Azemar <joel.azemar@gmail.com>
2022-02-07 11:29:55 +0100

    Initial Commit

---
+++ spec/mixin_predicate_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe MixinPredicate do
+  it "has a version number" do
+    expect(MixinPredicate::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
