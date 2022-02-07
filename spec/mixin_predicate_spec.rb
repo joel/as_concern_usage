292c3bccb76b38efc4106a22a00d624f1811ab5f
Joel Azemar <joel.azemar@gmail.com>
2022-02-07 11:30:05 +0100

    Change Spec

--- spec/mixin_predicate_spec.rb
+++ spec/mixin_predicate_spec.rb
@@ -1,11 +1,1 @@
-# frozen_string_literal: true
-
-RSpec.describe MixinPredicate do
-  it "has a version number" do
-    expect(MixinPredicate::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
+RSpec.describe MixinPredicate
