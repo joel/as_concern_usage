8ae85fc2af805f7cc8e483d209d7464a9e913d48
Joel Azemar <joel.azemar@gmail.com>
2022-02-07 11:30:12 +0100

    Rubocop

--- lib/mixin_predicate/main.rb
+++ lib/mixin_predicate/main.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module MixinPredicate
   class Main
     def mixin_predicate(input)
--- spec/mixin_predicate/main_spec.rb
+++ spec/mixin_predicate/main_spec.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
 module MixinPredicate
   RSpec.describe Main do
     subject(:foo) { described_class.new }
-    describe '#mixin_predicate' do
-      context 'when all is good' do
-        let(:input) { 'foo' }
-        let(:result) { 'foo' }
+    describe "#mixin_predicate" do
+      context "when all is good" do
+        let(:input) { "foo" }
+        let(:result) { "foo" }
+
         it do
           expect(foo.mixin_predicate(input)).to eql(result)
         end
